7c0fb6c0c3cf16aca82a02b88b25009fbba70c2d
Jonathan Alcántara <jalcantara1990@gmail.com>
2021-01-12 09:47:52 +0100

    Auto-generated commit

--- src/app/shared/user.model.ts
+++
@@ -1,9 +0,0 @@
-export let userIndex = 0;
-
-export class User {
-  constructor(
-    public id: number = userIndex++,
-    public name: string = 'Jonathan Alcántara',
-    public title: string = 'Software engineer'
-  ) {}
-}
